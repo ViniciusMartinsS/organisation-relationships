572bff39f2bf886d6231c089b1cb5071d71473ff
Vinicius Martins <visimonemartins@hotmail.com>
2022-01-15 20:55:25 +0000

    master Addition of FindByOrg method on repository

--- src/infrastructure/database/organisation.repository.ts
+++ src/infrastructure/database/organisation.repository.ts
@@ -41,6 +41,49 @@ class OrganisationRepository {
     await this.connection
       .query(query) as any
   }
+
+  public async findByOrganisation(organisation: string): Promise<any> {
+    const promises = []
+
+    let query = this.prepareFindQuery(organisation, 'branch')
+    promises.push(this.connection.query(query))
+
+    query = this.prepareFindQuery(organisation, 'headquarter')
+    promises.push(this.connection.query(query))
+
+    const [ [ branches ], [ headquarters ] ] = await Promise.all(promises)
+
+    if (!headquarters.length){
+      return { branches }
+    }
+
+    const parent = headquarters.map(({ name }) => {
+      const query = this.prepareFindQuery(name, 'branch', organisation)
+      return this.connection.query(query)
+    })
+
+    const [ sisters ] = await Promise.all(parent)as any
+    console.log(sisters)
+
+    return { branches, headquarters } // ...(sisters.length && { sisters })
+  }
+
+  private prepareFindQuery(
+    organisation: string,
+    type: string,
+    current_organization: string = organisation
+  ): string {
+    const FIRST_JOIN = { headquarter: 'branch_id', branch: 'headquarter_id' }[type]
+    const SECOND_JOIN = { headquarter: 'headquarter_id', branch: 'branch_id' }[type]
+
+    return ` SELECT oo.name FROM organisation AS o
+      RIGHT JOIN organisation_branch AS ob
+      ON o.id = ob.${FIRST_JOIN}
+      LEFT JOIN organisation AS oo
+      ON ob.${SECOND_JOIN} = oo.id
+      WHERE o.name = "${organisation}" AND oo.name != "${current_organization}";
+    `
+  }
 }
 
 export default OrganisationRepository
